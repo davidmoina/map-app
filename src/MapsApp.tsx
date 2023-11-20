ed264de75e9c2cf057301ad8b09e73f561558d23
davidmoina <luisdavidmoina@gmail.com>
2023-11-20 12:48:24 +0100

    🚧 Create places context

---
+++ src/MapsApp.tsx
@@ -0,0 +1,9 @@
+import { PlacesProvider } from "./context";
+
+export const MapsApp = () => {
+  return (
+    <PlacesProvider>
+      <h1>Hello world</h1>
+    </PlacesProvider>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { MapsApp } from "./MapsApp.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <MapsApp />
   </React.StrictMode>
 );
