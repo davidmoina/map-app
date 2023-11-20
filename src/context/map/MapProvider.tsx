61bd8cc584fa3cafdea1c74031a8c0004748955f
davidmoina <luisdavidmoina@gmail.com>
2023-11-20 18:42:03 +0100

    ✨ Place markers of the found places

--- src/context/map/MapProvider.tsx
+++ src/context/map/MapProvider.tsx
@@ -1,16 +1,19 @@
 import { Map, Marker, Popup } from "mapbox-gl";
 import { MapContext } from "./MapContext";
-import { useReducer } from "react";
+import { useContext, useEffect, useReducer } from "react";
 import { mapReducer } from "./MapReducer";
+import { PlacesContext } from "..";
 
 export interface MapState {
   isMapReady: boolean;
   map?: Map;
+  markers: Marker[];
 }
 
 const INITIAL_STATE: MapState = {
   isMapReady: false,
   map: undefined,
+  markers: [],
 };
 
 interface Props {
@@ -19,6 +22,31 @@ interface Props {
 
 export const MapProvider = ({ children }: Props) => {
   const [state, dispatch] = useReducer(mapReducer, INITIAL_STATE);
+
+  const { places } = useContext(PlacesContext);
+
+  useEffect(() => {
+    state.markers.forEach((marker) => marker.remove());
+    const newMarkers: Marker[] = [];
+
+    for (const place of places) {
+      const [lng, lat] = place.center;
+      const popUp = new Popup().setHTML(`
+      <h6>${place.text_es}</h6>
+      <p>${place.place_name_es}</p>
+      `);
+
+      const newMarker = new Marker()
+        .setPopup(popUp)
+        .setLngLat([lng, lat])
+        .addTo(state.map!);
+
+      newMarkers.push(newMarker);
+    }
+
+    dispatch({ type: "setMarkers", payload: newMarkers });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [places]);
 
   const setMap = (map: Map) => {
     const myLocationPopup = new Popup().setHTML(`
--- src/context/map/MapReducer.ts
+++ src/context/map/MapReducer.ts
@@ -1,7 +1,9 @@
-import { Map } from "mapbox-gl";
+import { Map, Marker } from "mapbox-gl";
 import { MapState } from "./MapProvider";
 
-type MapAction = { type: "setMap"; payload: Map };
+type MapAction =
+  | { type: "setMap"; payload: Map }
+  | { type: "setMarkers"; payload: Marker[] };
 
 export const mapReducer = (state: MapState, action: MapAction): MapState => {
   switch (action.type) {
@@ -10,6 +12,12 @@ export const mapReducer = (state: MapState, action: MapAction): MapState => {
         ...state,
         isMapReady: true,
         map: action.payload,
+      };
+
+    case "setMarkers":
+      return {
+        ...state,
+        markers: action.payload,
       };
 
     default:
